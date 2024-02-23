44d6720f45519b64a5c049119af566d2dabb23d8
Nikita Kolmogorov <backmeupplz@gmail.com>
2024-02-23 12:48:11 -0800

    use erc1155

--- contracts/KnockKnock.sol
+++ contracts/KnockKnock.sol
@@ -2,24 +2,25 @@
 pragma solidity ^0.8.23;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/interfaces/IERC1155.sol";
 import "./IWithBalanceOf.sol";
 
 contract KnockKnock is Ownable {
-  IWithBalanceOf public hostContract;
+  IERC1155 public hostContract;
 
   event Knock(address indexed _from);
 
   constructor(address _hostContract) Ownable(msg.sender) {
-    hostContract = IWithBalanceOf(_hostContract);
+    hostContract = IERC1155(_hostContract);
   }
 
   function setHostContract(address _hostContract) external onlyOwner {
-    hostContract = IWithBalanceOf(_hostContract);
+    hostContract = IERC1155(_hostContract);
   }
 
   function knock() external {
     require(
-      hostContract.balanceOf(msg.sender) > 0,
+      hostContract.balanceOf(msg.sender, 1) > 0,
       "You don't have any balance"
     );
     emit Knock(msg.sender);
